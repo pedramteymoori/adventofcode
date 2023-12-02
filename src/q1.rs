df54c9e10f6ae912cd45c385553cd4281ba11b3a
Pedram Teymoori <pedramteymoori@gmail.com>
2023-12-02 01:01:36 +0100

    q1 improved version

--- src/q1.rs
+++ src/q1.rs
@@ -1,30 +1,43 @@
 
-use std::io::{self, BufRead};
+use std::io;
     pub fn run() {
-        let stdin = io::stdin();
-
         let mut sum = 0;
-        for line in stdin.lock().lines() {
-            let str = line.unwrap();
-            if str == ";" {
+
+        loop{
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).expect("failed to read line");
+
+            let input_line = input_line.trim();
+
+            if input_line == ";" {
                 break
             }
+            // shadowed
 
             let mut first: i32 = -1;
             let mut last: i32 = -1;
 
-            for c in str.chars() {
+            for c in input_line.chars() {
                 if !c.is_digit(10) {
                     continue
                 }
 
-                let digit_value = c.to_digit(10).unwrap() as i32;
+                let digit_value = match c.to_digit(10){
+                    Some(d) => d as i32,
+                    None => {
+                        println!("cannot cast into a number");
+                        continue
+                    }
+                };
                 last = digit_value;
                 if first == -1 {
                     first = digit_value;
                 }
             }
-            sum += (first * 10) + last;
+            if first != -1 && last != -1 {
+                sum += (first * 10) + last;
+            }
+
         }
         println!("{}", sum);
     }
